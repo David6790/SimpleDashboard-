f6b6f1a498fdab2237a8e31810e25035579456c6
David6790 <david.lb90@gmail.com>
2024-06-10 17:20:52 +0200

    route get ok quasiement

--- src/pages/ModalPlan.jsx
+++ src/pages/ModalPlan.jsx
@@ -26,19 +26,20 @@ const ModalPlan = ({ reservation, closeModal }) => {
   useEffect(() => {
     if (allocations) {
       console.log("Allocations:", allocations);
-      const occupied = allocations.map((allocation) => allocation.table.name);
+      const occupied = allocations.map((allocation) => ({
+        tableName: allocation.table.name,
+        clientPrenom: allocation.reservation.clientPrenom,
+        timeResa: allocation.reservation.timeResa,
+        numberOfGuest: allocation.reservation.numberOfGuest,
+      }));
       setOccupiedTables(occupied);
     }
   }, [allocations]);
 
   const handleTableClick = (table) => {
-    // Vérifie d'abord si la table est occupée
     if (isOccupied(table)) {
-      // Si la table est occupée, ne fais rien
       return;
     }
-
-    // Ensuite, gère la logique de sélection comme avant
     if (isMultiTableMode) {
       setSelectedTables((prevSelectedTables) =>
         prevSelectedTables.includes(table)
@@ -53,12 +54,11 @@ const ModalPlan = ({ reservation, closeModal }) => {
   };
 
   const isSelected = (table) => selectedTables.includes(table);
-  const isOccupied = (table) => occupiedTables.includes(table);
-
-  const handleMultiTableToggle = () => setIsMultiTableMode(!isMultiTableMode);
+  const isOccupied = (table) =>
+    occupiedTables.some((occupied) => occupied.tableName === table);
 
   const getTableClass = (table) => {
-    return `table border-4 shadow-lg px-10 py-10 ${
+    return `table border-4 shadow-lg  flex items-end justify-center text-md ${
       isSelected(table)
         ? "bg-blue-300"
         : isOccupied(table)
@@ -67,8 +67,20 @@ const ModalPlan = ({ reservation, closeModal }) => {
     }`;
   };
 
+  const getOccupiedTableInfo = (table) => {
+    const occupiedTable = occupiedTables.find(
+      (occupied) => occupied.tableName === table
+    );
+    if (occupiedTable) {
+      return `${occupiedTable.clientPrenom}, ${occupiedTable.timeResa}, ${occupiedTable.numberOfGuest}p`;
+    }
+    return null;
+  };
+
+  const handleMultiTableToggle = () => setIsMultiTableMode(!isMultiTableMode);
+
   return (
-    <div className="w-full px-10 mb-14 z-50">
+    <div className="w-full px-10 mb-14 z-50 ">
       <div className="pt-10 flex justify-between mb-4">
         <label className="flex items-center space-x-2">
           <input
@@ -82,29 +94,42 @@ const ModalPlan = ({ reservation, closeModal }) => {
           Confirmer les Tables Sélectionnées ({selectedTables.length})
         </button>
       </div>
+
       <div className="overflow-x-auto">
         <div className="pt-10 flex flex-row justify-between min-w-[1000px]">
           <div className="flex flex-row w-1/4 justify-start gap-5">
             {["7", "8"].map((table) => (
-              <div
-                key={table}
-                id={table}
-                className={getTableClass(table, 98)}
-                onClick={() => handleTableClick(table)}
-              >
-                {table}
+              <div key={table} className="relative">
+                {isOccupied(table) && (
+                  <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-yellow-300 px-4 py-2 rounded-full shadow-lg text-xs font-bold">
+                    {getOccupiedTableInfo(table)}
+                  </div>
+                )}
+                <div
+                  id={table}
+                  className={getTableClass(table)}
+                  onClick={() => handleTableClick(table)}
+                >
+                  {table}
+                </div>
               </div>
             ))}
           </div>
           <div className="flex flex-row w-3/4 justify-end gap-5">
             {["9", "10", "11", "12", "13", "14"].map((table) => (
-              <div
-                key={table}
-                id={table}
-                className={getTableClass(table, 98)}
-                onClick={() => handleTableClick(table)}
-              >
-                {table}
+              <div key={table} className="relative">
+                {isOccupied(table) && (
+                  <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-yellow-300 px-4 py-2 rounded-full shadow-lg text-xs font-bold">
+                    {getOccupiedTableInfo(table)}
+                  </div>
+                )}
+                <div
+                  id={table}
+                  className={getTableClass(table)}
+                  onClick={() => handleTableClick(table)}
+                >
+                  {table}
+                </div>
               </div>
             ))}
           </div>
@@ -112,25 +137,37 @@ const ModalPlan = ({ reservation, closeModal }) => {
         <div className="pt-10 flex flex-row justify-between min-w-[1000px]">
           <div className="flex flex-row w-1/4 justify-start">
             {["6"].map((table) => (
-              <div
-                key={table}
-                id={table}
-                className={getTableClass(table, 98)}
-                onClick={() => handleTableClick(table)}
-              >
-                {table}
+              <div key={table} className="relative">
+                {isOccupied(table) && (
+                  <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-yellow-300 px-4 py-2 rounded-full shadow-lg text-xs font-bold">
+                    {getOccupiedTableInfo(table)}
+                  </div>
+                )}
+                <div
+                  id={table}
+                  className={getTableClass(table)}
+                  onClick={() => handleTableClick(table)}
+                >
+                  {table}
+                </div>
               </div>
             ))}
           </div>
           <div className="flex flex-row w-2/3 justify-end">
             {["15"].map((table) => (
-              <div
-                key={table}
-                id={table}
-                className={getTableClass(table, 98)}
-                onClick={() => handleTableClick(table)}
-              >
-                {table}
+              <div key={table} className="relative">
+                {isOccupied(table) && (
+                  <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-yellow-300 px-4 py-2 rounded-full shadow-lg text-xs font-bold">
+                    {getOccupiedTableInfo(table)}
+                  </div>
+                )}
+                <div
+                  id={table}
+                  className={getTableClass(table)}
+                  onClick={() => handleTableClick(table)}
+                >
+                  {table}
+                </div>
               </div>
             ))}
           </div>
@@ -138,25 +175,37 @@ const ModalPlan = ({ reservation, closeModal }) => {
         <div className="pt-10 flex flex-row justify-between min-w-[1000px]">
           <div className="flex flex-row w-1/3 justify-between">
             {["5", "20"].map((table) => (
-              <div
-                key={table}
-                id={table}
-                className={getTableClass(table, 98)}
-                onClick={() => handleTableClick(table)}
-              >
-                {table}
+              <div key={table} className="relative">
+                {isOccupied(table) && (
+                  <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-yellow-300 px-4 py-2 rounded-full shadow-lg text-xs font-bold">
+                    {getOccupiedTableInfo(table)}
+                  </div>
+                )}
+                <div
+                  id={table}
+                  className={getTableClass(table)}
+                  onClick={() => handleTableClick(table)}
+                >
+                  {table}
+                </div>
               </div>
             ))}
           </div>
           <div className="flex flex-row w-2/3 justify-end gap-5">
             {["19", "18", "16"].map((table) => (
-              <div
-                key={table}
-                id={table}
-                className={getTableClass(table, 98)}
-                onClick={() => handleTableClick(table)}
-              >
-                {table}
+              <div key={table} className="relative">
+                {isOccupied(table) && (
+                  <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-yellow-300 px-4 py-2 rounded-full shadow-lg text-xs font-bold">
+                    {getOccupiedTableInfo(table)}
+                  </div>
+                )}
+                <div
+                  id={table}
+                  className={getTableClass(table)}
+                  onClick={() => handleTableClick(table)}
+                >
+                  {table}
+                </div>
               </div>
             ))}
           </div>
@@ -164,25 +213,37 @@ const ModalPlan = ({ reservation, closeModal }) => {
         <div className="pt-10 flex flex-row justify-between min-w-[1000px]">
           <div className="flex flex-row w-1/4 justify-start">
             {["4"].map((table) => (
-              <div
-                key={table}
-                id={table}
-                className={getTableClass(table, 98)}
-                onClick={() => handleTableClick(table)}
-              >
-                {table}
+              <div key={table} className="relative">
+                {isOccupied(table) && (
+                  <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-yellow-300 px-4 py-2 rounded-full shadow-lg text-xs font-bold">
+                    {getOccupiedTableInfo(table)}
+                  </div>
+                )}
+                <div
+                  id={table}
+                  className={getTableClass(table)}
+                  onClick={() => handleTableClick(table)}
+                >
+                  {table}
+                </div>
               </div>
             ))}
           </div>
           <div className="flex flex-row w-2/3 justify-end">
             {["17"].map((table) => (
-              <div
-                key={table}
-                id={table}
-                className={getTableClass(table, 98)}
-                onClick={() => handleTableClick(table)}
-              >
-                {table}
+              <div key={table} className="relative">
+                {isOccupied(table) && (
+                  <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-yellow-300 px-4 py-2 rounded-full shadow-lg text-xs font-bold">
+                    {getOccupiedTableInfo(table)}
+                  </div>
+                )}
+                <div
+                  id={table}
+                  className={getTableClass(table)}
+                  onClick={() => handleTableClick(table)}
+                >
+                  {table}
+                </div>
               </div>
             ))}
           </div>
@@ -190,13 +251,19 @@ const ModalPlan = ({ reservation, closeModal }) => {
         <div className="pt-10 flex flex-row justify-between min-w-[1000px]">
           <div className="flex flex-row w-2/3 justify-between gap-5">
             {["3", "22", "23", "24", "25", "26"].map((table) => (
-              <div
-                key={table}
-                id={table}
-                className={getTableClass(table, 98)}
-                onClick={() => handleTableClick(table)}
-              >
-                {table}
+              <div key={table} className="relative">
+                {isOccupied(table) && (
+                  <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-yellow-300 px-4 py-2 rounded-full shadow-lg text-xs font-bold">
+                    {getOccupiedTableInfo(table)}
+                  </div>
+                )}
+                <div
+                  id={table}
+                  className={getTableClass(table)}
+                  onClick={() => handleTableClick(table)}
+                >
+                  {table}
+                </div>
               </div>
             ))}
           </div>
@@ -204,13 +271,19 @@ const ModalPlan = ({ reservation, closeModal }) => {
         <div className="pt-10 flex flex-row justify-between min-w-[1000px]">
           <div className="flex flex-row w-1/3 justify-between gap-5">
             {["2", "2-BIS", "1-BIS", "1"].map((table) => (
-              <div
-                key={table}
-                id={table}
-                className={getTableClass(table, 98)}
-                onClick={() => handleTableClick(table)}
-              >
-                {table}
+              <div key={table} className="relative">
+                {isOccupied(table) && (
+                  <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-yellow-300 px-4 py-2 rounded-full shadow-lg text-xs font-bold">
+                    {getOccupiedTableInfo(table)}
+                  </div>
+                )}
+                <div
+                  id={table}
+                  className={getTableClass(table)}
+                  onClick={() => handleTableClick(table)}
+                >
+                  {table}
+                </div>
               </div>
             ))}
           </div>
